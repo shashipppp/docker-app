369352753680875debc1090211c7f8b153917113
kallanagoudar <venkanagouda.kallanagoudar@team.telstra.com>
2021-08-04 17:03:26 +0530

    Mongo service

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_IP, MONGO_USER, MONGO_PORT, MONGO_PASSWORD } = require('./config/config');
 
 const app = express();
+
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Mongo DB Sucessfuly started....'))
+  .catch(err => console.log(err));
 
 app.use('/', (req, res) => {
   res.send('Hi There....');
