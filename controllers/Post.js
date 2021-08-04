683af1b276e9a4aec89cd147b87b4f0a41a7e507
kallanagoudar <venkanagouda.kallanagoudar@team.telstra.com>
2021-08-04 21:22:14 +0530

    post routes

--- controllers/Post.js
+++ controllers/Post.js
@@ -4,13 +4,13 @@ exports.getAllPosts = async (req, res, next) => {
   try {
     const posts = await Post.find();
 
-    res.send(200).json({
+    res.status(200).json({
       status: 'Success',
       length: posts.length,
       data: posts,
     });
   }catch(e) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'Error',
       error: e,
     });
@@ -21,12 +21,12 @@ exports.createPost = async (req, res) => {
   try {
     const post = await Post.create(req.body);
 
-    res.send(200).json({
+    res.status(200).json({
       status: 'Success',
       data: post,
     });
   }catch(e) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'Error',
       error: e,
     });
@@ -37,12 +37,12 @@ exports.getPost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    res.send(200).json({
+    res.status(200).json({
       status: 'Success',
       data: post,
     });
   }catch(e) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'Error',
       error: e,
     });
@@ -56,12 +56,12 @@ exports.updatePost = async (req, res) => {
       runValidators: true,
     });
 
-    res.send(200).json({
+    res.status(200).json({
       status: 'Success',
       data: post,
     });
   }catch(e) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'Error',
       error: e,
     });
@@ -72,11 +72,11 @@ exports.deletePost = async (req, res) => {
   try {
     const post = await Post.findByIdAndDelete(req.params.id);
 
-    res.send(200).json({
+    res.status(200).json({
       status: 'Success',
     });
   }catch(e) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'Error',
       error: e,
     });
